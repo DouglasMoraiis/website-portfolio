852b48228a0fc490fe3e41ce204975ac4c9c0c9b
Douglas Morais <janderdouglas2014@gmail.com>
2021-12-30 16:13:06 -0300

    ✔️ Complete Qualification

--- src/containers/Qualification/Qualification.tsx
+++ src/containers/Qualification/Qualification.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { UilGraduationCap, UilBriefcaseAlt } from '@iconscout/react-unicons';
 
 import {
@@ -9,48 +11,79 @@ import {
   RoadmapGroup,
   RoadmapItem
 } from '../../components';
+import { myIconSizes } from '../../theme';
 import { useStyles } from './Qualification.style';
 
 function Qualification() {
   const classes = useStyles();
+  const [statusButton1, setStatusButton1] = useState(true);
+  const [statusButton2, setStatusButton2] = useState(false);
+
+  const handleButton1 = () => {
+    setStatusButton1(true);
+    setStatusButton2(false);
+  };
+
+  const handleButton2 = () => {
+    setStatusButton2(true);
+    setStatusButton1(false);
+  };
 
   return (
-    <Section id="qualification" className={classes.qualification}>
+    <Section
+      id="qualification"
+      className={`${classes.qualification} container`}
+    >
       <header>
-        <h2>About Me</h2>
-        <span>My Introduction</span>
+        <h2>Qualification</h2>
+        <span>My personal journey</span>
       </header>
-
       <Roadmap>
         <RoadmapNav>
-          <RoadmapNavButton title="Education" icon={<UilGraduationCap />} />
-          <RoadmapNavButton title="Work" icon={<UilBriefcaseAlt />} />
+          <RoadmapNavButton
+            title="Education"
+            icon={<UilGraduationCap size={myIconSizes.normal} />}
+            status={statusButton1}
+            onClick={handleButton1}
+          />
+          <RoadmapNavButton
+            title="Work"
+            icon={<UilBriefcaseAlt size={myIconSizes.normal} />}
+            status={statusButton2}
+            onClick={handleButton2}
+          />
         </RoadmapNav>
 
         <RoadmapContent>
-          <RoadmapGroup id="education">
+          <RoadmapGroup id="education" status={statusButton1}>
             <RoadmapItem
-              title="Graduando em Ciência da Computação"
-              subtitle="UFC - Campus Russas"
-              calendar="2018 - 2022"
+              title="Técnico em Redes de Computadores"
+              subtitle="EP Júlio França - Ceará"
+              calendar="2015 - 2017"
+              side="left"
             />
             <RoadmapItem
               title="Graduando em Ciência da Computação"
-              subtitle="UFC - Campus Russas"
+              subtitle="Universidade Federal do Ceará"
               calendar="2018 - 2022"
+              side="right"
+              last
             />
           </RoadmapGroup>
 
-          <RoadmapGroup id="work">
+          <RoadmapGroup id="work" status={statusButton2}>
             <RoadmapItem
-              title="Graduando em Ciência da Computação"
-              subtitle="UFC - Campus Russas"
-              calendar="2018 - 2022"
+              title="Estágio como Design Gráfico"
+              subtitle="Rezult - Marketing Digital"
+              calendar="2017"
+              side="left"
             />
             <RoadmapItem
-              title="Graduando em Ciência da Computação"
-              subtitle="UFC - Campus Russas"
-              calendar="2018 - 2022"
+              title="Desenvolvedor Front-end"
+              subtitle="Fundação ASTEF"
+              calendar="Desde 2021"
+              side="right"
+              last
             />
           </RoadmapGroup>
         </RoadmapContent>
