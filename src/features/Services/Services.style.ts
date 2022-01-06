089a856c1814ea01a6660edb069dd67e8ef5effd
Douglas Morais <janderdouglas2014@gmail.com>
2022-01-06 19:32:06 -0300

    Create Services feature

--- src/features/Services/Services.style.ts
+++ src/features/Services/Services.style.ts
@@ -3,9 +3,32 @@ import { makeStyles } from '@material-ui/core';
 import { myColors, myFonts, myHeights, myMarginBottom } from '../../theme';
 
 export const useStyles = makeStyles({
-  services__container: {},
-  services__content: {},
-  services__button: {},
-  services__modal_services: {},
-  services__modal_service: {}
+  services: {},
+  services__container: {
+    gap: '1.5rem',
+    gridTemplateColumns: 'repeat(2, 1fr)'
+  },
+  services__modal_ul: {
+    padding: 0,
+    '& p': {
+      margin: 0
+    }
+  },
+  services__modal_li: {
+    gap: '.3rem',
+    padding: 0,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  services__button: {
+    display: 'flex',
+    justifyContent: 'center',
+    transition: '.3s',
+    '& svg': {
+      transition: '.3s'
+    },
+    '&:hover svg': {
+      transform: 'translateX(0.25rem)'
+    }
+  }
 });
--- src/features/Services/Services.tsx
+++ src/features/Services/Services.tsx
@@ -1,52 +1,171 @@
+import React, { useState } from 'react';
+
 import {
   UilWebGrid,
   UilCheckCircle,
-  UilInstagram,
-  UilMessage,
-  UilMouseAlt,
+  UilArrow,
+  UilPen,
   UilArrowRight
 } from '@iconscout/react-unicons';
 
 import { ButtonLink, Section, Card, Modal } from '../../components';
+import { myIconSizes } from '../../theme';
 import { useStyles } from './Services.style';
 
 function Services() {
   const classes = useStyles();
 
+  const [stateModals, setStateModals] = useState({
+    modalUIUX: false,
+    modalFront: false,
+    modalBranding: false
+  });
+
+  const handleActiveModal = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const { id } = event.currentTarget;
+    switch (id) {
+      case 'buttonCardUiUx':
+        setStateModals({ ...stateModals, modalUIUX: true });
+        break;
+      case 'buttonCardFront':
+        setStateModals({ ...stateModals, modalFront: true });
+        break;
+      case 'buttonCardBranding':
+        setStateModals({ ...stateModals, modalBranding: true });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDisableModal = () => {
+    setStateModals({
+      modalUIUX: false,
+      modalFront: false,
+      modalBranding: false
+    });
+  };
+
   return (
-    <Section id="services" className={`${classes}`}>
+    <Section id="services" className={`${classes.services} container`}>
       <header>
         <h2>Services</h2>
         <span>What i offer</span>
       </header>
 
-      <Card title="Ui/Ux Designer" iconTitle={<UilWebGrid />}>
-        <ButtonLink href="#" className={classes.services__button}>
-          <span>ViewMore</span>
-          <UilArrowRight />
-        </ButtonLink>
+      <div className={`${classes.services__container} container grid`}>
+        <Card
+          title="Ui/Ux Designer"
+          iconTitle={<UilWebGrid size={myIconSizes.big} />}
+        >
+          <span className={classes.services__button}>
+            <ButtonLink onClick={handleActiveModal} id="buttonCardUiUx">
+              <span>ViewMore</span>
+              <UilArrowRight size={myIconSizes.small} />
+            </ButtonLink>
+          </span>
 
-        <Modal>
-          <ul className={`${classes.services__modal_services} grid`}>
-            <li className={classes.services__modal_service}>
-              <UilCheckCircle />
-              <p>Lorem ipsum dolor sit amet.</p>
-            </li>
-            <li className={classes.services__modal_service}>
-              <UilCheckCircle />
-              <p>Lorem ipsum dolor sit amet.</p>
-            </li>
-            <li className={classes.services__modal_service}>
-              <UilCheckCircle />
-              <p>Lorem ipsum dolor sit amet.</p>
-            </li>
-            <li className={classes.services__modal_service}>
-              <UilCheckCircle />
-              <p>Lorem ipsum dolor sit amet.</p>
-            </li>
-          </ul>
-        </Modal>
-      </Card>
+          <Modal
+            title="Ui/Ux Designer"
+            status={stateModals.modalUIUX}
+            disable={handleDisableModal}
+          >
+            <ul className={`${classes.services__modal_ul} grid`}>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+            </ul>
+          </Modal>
+        </Card>
+
+        <Card
+          title="Frontend Developer"
+          iconTitle={<UilArrow size={myIconSizes.big} />}
+        >
+          <span className={classes.services__button}>
+            <ButtonLink onClick={handleActiveModal} id="buttonCardFront">
+              <span>ViewMore</span>
+              <UilArrowRight size={myIconSizes.small} />
+            </ButtonLink>
+          </span>
+
+          <Modal
+            title="Frontend Developer"
+            status={stateModals.modalFront}
+            disable={handleDisableModal}
+          >
+            <ul className={`${classes.services__modal_ul} grid`}>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+            </ul>
+          </Modal>
+        </Card>
+
+        <Card
+          title="Branding Designer"
+          iconTitle={<UilPen size={myIconSizes.big} />}
+        >
+          <span className={classes.services__button}>
+            <ButtonLink onClick={handleActiveModal} id="buttonCardBranding">
+              <span>ViewMore</span>
+              <UilArrowRight size={myIconSizes.small} />
+            </ButtonLink>
+          </span>
+
+          <Modal
+            title="Branding Designer"
+            status={stateModals.modalBranding}
+            disable={handleDisableModal}
+          >
+            <ul className={`${classes.services__modal_ul} grid`}>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+              <li className={classes.services__modal_li}>
+                <UilCheckCircle size={myIconSizes.small} />
+                <p>Lorem ipsum dolor sit amet.</p>
+              </li>
+            </ul>
+          </Modal>
+        </Card>
+      </div>
     </Section>
   );
 }
