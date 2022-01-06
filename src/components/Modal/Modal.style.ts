d768067f2f58c829a1f9eea486e2bacc32dce68f
Douglas Morais <janderdouglas2014@gmail.com>
2022-01-06 19:30:24 -0300

    Create Modal component

--- src/components/Modal/Modal.style.ts
+++ src/components/Modal/Modal.style.ts
@@ -1,6 +1,40 @@
 import { makeStyles } from '@material-ui/core';
 
+import { myColors, myZIndex } from '../../theme';
+
 export const useStyles = makeStyles({
-  content: {},
-  title: {}
+  container: {
+    position: 'fixed',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: 'rgba(0,0,0,.5)',
+    zIndex: myZIndex.zModal,
+    padding: '0 1rem',
+    opacity: 0,
+    visibility: 'hidden',
+    transition: '.3s'
+  },
+  content: {
+    position: 'relative',
+    background: myColors.containerColor,
+    padding: '1.5rem',
+    borderRadius: '.5rem',
+    '& a': {
+      cursor: 'pointer'
+    }
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  active: {
+    transition: '.3s',
+    opacity: 1,
+    visibility: 'visible'
+  }
 });
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 
 import { UilTimes } from '@iconscout/react-unicons';
 
 import { useStyles } from './Modal.style';
 
 type ModalProps = {
+  title: string;
+  status: boolean;
+  disable: () => void;
   children: React.ReactElement | Array<React.ReactElement>;
 };
 
-function Modal({ children }: ModalProps) {
+function Modal({ title, status, disable, children }: ModalProps) {
   const classes = useStyles();
+
+  const [closeModal, setCloseModal] = useState(false);
+
   return (
-    <div className={classes.content}>
-      <UilTimes />
-      {children}
+    <div className={`${status ? classes.active : ''} ${classes.container}`}>
+      <div className={classes.content}>
+        <div className={classes.title}>
+          <h4>{title}</h4>
+          <a onClick={disable}>
+            <UilTimes />
+          </a>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
