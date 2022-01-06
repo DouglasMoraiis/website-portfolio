78c86dcf75634e34e2747fedbf241a9f81b02e90
Douglas Morais <janderdouglas2014@gmail.com>
2022-01-06 19:29:31 -0300

    upgrade props component ButtonLink

--- src/components/ButtonLink/ButtonLink.style.ts
+++ src/components/ButtonLink/ButtonLink.style.ts
@@ -1,8 +1,15 @@
 import { makeStyles } from '@material-ui/core';
 
+import { myColors } from '../../theme';
+
 export const useStyles = makeStyles({
   button_flex: {
     display: 'inline-flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    color: myColors.firstColor,
+    cursor: 'pointer',
+    '&:hover': {
+      color: myColors.firstColorAlt
+    }
   }
 });
--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -3,23 +3,34 @@ import React from 'react';
 import { useStyles } from './ButtonLink.style';
 
 type ButtonLinkProps = {
-  href: string | undefined;
-  className?: string;
+  id?: string;
+  href?: string;
   target?: string;
+  className?: string;
   children: React.ReactElement | Array<React.ReactElement> | undefined;
+  onClick?: (event: React.MouseEvent) => void;
 };
 
-function ButtonLink({ href, className, target, children }: ButtonLinkProps) {
+function ButtonLink({
+  id,
+  href,
+  target,
+  className,
+  children,
+  onClick
+}: ButtonLinkProps) {
   const classes = useStyles();
 
   const rel = target ? 'noreferrer noopener' : '';
 
   return (
     <a
+      id={id}
       href={href}
-      className={`${className} ${classes.button_flex}`}
+      className={`${className} ${classes.button_flex} `}
       target={target}
       rel={rel}
+      onClick={onClick}
     >
       {children}
     </a>
