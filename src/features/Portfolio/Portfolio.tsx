289e33a9d6ee304fdffbbb75748e37c7fb66b304
Douglas Morais <janderdouglas2014@gmail.com>
2022-01-07 12:28:53 -0300

    Create Portfolio feature

--- src/features/Portfolio/Portfolio.tsx
+++ src/features/Portfolio/Portfolio.tsx
@@ -1,14 +1,25 @@
 import { UilFileDownloadAlt } from '@iconscout/react-unicons';
 
-import { Button, Section } from '../../components';
+import { Button, Section, Carrossel, CarrosselItem } from '../../components';
 import { myIconSizes } from '../../theme';
 import { useStyles } from './Portfolio.style';
 
 function Portfolio() {
   const classes = useStyles();
   return (
-    <Section id="about" className={`${classes.about} container`}>
-      <h1>h1</h1>
+    <Section id="portifolio" className={`${classes.about} container`}>
+      <header>
+        <h2>Portfolio</h2>
+        <span>My recents works</span>
+      </header>
+
+      <Carrossel id="carrosselPortifolio">
+        <CarrosselItem
+          id="modernWebsite"
+          title="Modern Website"
+          description="Website adaptable to all devices, with and animated interactions"
+        />
+      </Carrossel>
     </Section>
   );
 }
