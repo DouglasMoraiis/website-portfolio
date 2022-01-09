f5bf5fd8aaec971b04ada922f04b4419f5666ac0
Douglas Morais <janderdouglas2014@gmail.com>
2022-01-09 13:18:33 -0300

    Refatoring header Section component

--- src/components/Section/Section.style.ts
+++ src/components/Section/Section.style.ts
@@ -9,32 +9,18 @@ import {
 } from '../../theme';
 
 export const useStyles = makeStyles({
-  section: {
-    padding: '2rem 0 4rem',
-    '& header': {
-      '& h2': {
-        fontSize: myFonts.fontSize.h1,
-        textAlign: 'center',
-        margin: '0'
-      },
-      '& span': {
-        display: 'block',
-        fontSize: myFonts.fontSize.small,
-        textAlign: 'center',
-        marginBottom: myMarginBottom.mb3
-      }
-    }
+  container: {
+    padding: '2rem 0 4rem'
+  },
+  headerTitle: {
+    fontSize: myFonts.fontSize.h1,
+    textAlign: 'center',
+    margin: '0'
+  },
+  headerSubtitle: {
+    display: 'block',
+    fontSize: myFonts.fontSize.small,
+    textAlign: 'center',
+    marginBottom: myMarginBottom.mb3
   }
-  // section__title: {
-  //     display: 'block',
-  //     fontSize: myFonts.fontSize.small,
-  //     textAlign: 'center',
-  //     marginBottom: myMarginBottom.mb3
-  // },
-  // section__subtitle: {
-  //     display: 'block',
-  //     fontSize: myFonts.fontSize.small,
-  //     textAlign: 'center',
-  //     marginBottom: myMarginBottom.mb3
-  // },
 });
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -3,16 +3,22 @@ import React from 'react';
 import { useStyles } from './Section.style';
 
 type SectionProps = {
-  id: string;
-  className: string | undefined;
-  children: React.ReactElement | Array<React.ReactElement>;
+  id?: string;
+  className?: string;
+  title?: string;
+  subtitle?: string;
+  children: React.ReactNode;
 };
 
-function Section({ id, className, children }: SectionProps) {
+function Section({ id, className, title, subtitle, children }: SectionProps) {
   const classes = useStyles();
 
   return (
-    <section id={id} className={`${className} ${classes.section}`}>
+    <section id={id} className={`${className} ${classes.container}`}>
+      <header>
+        <h2 className={classes.headerTitle}>{title}</h2>
+        <span className={classes.headerSubtitle}>{subtitle}</span>
+      </header>
       {children}
     </section>
   );
